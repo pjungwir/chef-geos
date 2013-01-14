2e3865e3a1e2318069b86e9f8a135ed86bb0367a
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2013-01-14 15:48:58 -0800

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+maintainer        "Paul A. Jungwirth"
+maintainer_email  "pj@illuminatedcomputing.com"
+license           "Apache 2.0"
+description       "Installs GEOS"
+version           "0.1"
+
+recipe "geos", "Installs GEOS"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
+
