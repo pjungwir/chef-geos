8fb13345b96b12f664d8aff3618cec7fec6bb3cc
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2013-01-17 20:49:08 -0800

    version bump to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Paul A. Jungwirth"
 maintainer_email  "pj@illuminatedcomputing.com"
 license           "Apache 2.0"
 description       "Installs GEOS"
-version           "0.1"
+version           "0.1.1"
 
 recipe "geos", "Installs GEOS"
 
